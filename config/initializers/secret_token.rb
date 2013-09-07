aa39d017daccb3c48d4dcea75a7b16efe144e5f0
Feifan Zhou <feifan@me.com>
2013-09-06 21:15:24 -0400

    Initial Rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Orange::Application.config.secret_token = '6f8e21f5b19bf1bf70b5697bd22bf996981edb4a11e9ece9a1893d8632d0416f72ec900ea35cf39256b77153fa2b86e194ad2782709cef98fe1dc3da305af99f'
